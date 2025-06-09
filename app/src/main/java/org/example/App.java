f5f7e93bb5dae455ce52bf47efe57a2600da4f31
Sean <zerog133@yahoo.com>
2025-06-08 22:36:02 -0500

    Completed assignment

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,31 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
-  public int getInput() {
-    // Implement your function here
+  public static int getInput() {
+    System.out.println("Please enter a value");
+    Scanner input = new Scanner(System.in);
+    int num = input.nextInt();
+    while (!inputIsValid(num)){
+      System.out.println("Your value is invalid");
+      System.out.println("Please enter a value");
+      num = input.nextInt();
+    }
+    System.out.println("The value chosen by the user is " + num);
+    input.close();
+    return 0;
   }
 
+  public static boolean inputIsValid(int num){
+    if (num < 0 || num > 100){
+      return false;
+    }
+    
+    return true;
+  }
   public static void main(String[] args) {
     // Test your function here in a driver program
+    getInput();
   }
 }
